479ab98e1597a718db439eb8e35e1cb4ffd5a4a6
Chris Snowden <cps15@ic.ac.uk>
2016-08-10 18:42:21 +0100

    Key

--- server/src/AnalysisEngine/Analytics/Aggregation/TopAuthors.py
+++ server/src/AnalysisEngine/Analytics/Aggregation/TopAuthors.py
@@ -32,7 +32,7 @@ class TopAuthors(Analytics):
             {"$group": {"_id": user_name_key, "count": {"$sum": 1}}},
             {"$sort": {"count": -1}},
             {"$limit": limit}]
-
+        print query
         data = self.col.aggregate(query, allowDiskUse=True)
 
         result = {"details": {"chartType": "bar2d",
