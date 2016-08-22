e445e1160adce43b87a81c7317018ab00487cd8a
Chris Snowden <cps15@ic.ac.uk>
2016-08-22 17:45:33 +0100

    Profile returns up to tweet limit

--- server/src/AnalysisEngine/UserProfiling/UserProfile.py
+++ server/src/AnalysisEngine/UserProfiling/UserProfile.py
@@ -2,8 +2,7 @@ import datetime
 
 class UserProfile(object):
 
-    def __init__(self, screenName, dataSetName, tweet_limit = 25):
-        self.tweet_limt = tweet_limit
+    def __init__(self, screenName, dataSetName):
         self.profileImage = None
         self.name = ""
         self.screenName = "@" + screenName
@@ -22,14 +21,14 @@ class UserProfile(object):
         self.location = ""
 
     def add_tweet(self, s):
-        if len(self.tweets) < self.tweet_limt:
-            entry = {"dt": s.get_created_at(),"text": s.get_text().replace("\n", "")}
-            if s.get_retweet_status() is not None:
-                entry["retweet_author"] = s.get_retweet_status().get_user(True).get_name()
-            else:
-                entry["retweet_author"] = ""
 
-            self.tweets.append(entry)
+        entry = {"dt": s.get_created_at(),"text": s.get_text().replace("\n", "")}
+        if s.get_retweet_status() is not None:
+            entry["retweet_author"] = s.get_retweet_status().get_user(True).get_name()
+        else:
+            entry["retweet_author"] = ""
+
+        self.tweets.append(entry)
 
         coordinates = s.get_coordinates()
         if coordinates is not None:
