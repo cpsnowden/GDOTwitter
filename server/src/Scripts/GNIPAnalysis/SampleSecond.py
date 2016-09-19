5f949058fbbeb1dd8c8cc6a3cbcfe79e445a2885
Chris Snowden <cps15@ic.ac.uk>
2016-09-19 16:06:00 +0100

    Sample Tweets

---
+++ server/src/Scripts/GNIPAnalysis/SampleSecond.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+
+# client = MongoClient("146.169.32.151", 27017)
+#
+# db = client.get_database("DATA")
+# auth = db.authenticate("twitterApplication", "gdotwitter", source="admin")
+# col = db.get_collection("Twitter_Brexit_GNIP")
+
+import json
+
+path = "gnip_top_users.json"
+
+with open(path) as f:
+    data = json.load(f)
+
+users = [i["_id"]["user-id"] for i in data[1000:2000]]
+# users = [i["_id"]["user-id"] for i in data if i["count"] >= 150]
+print len(users)
+print users[:10]
+
+a = raw_input("Press to continue")
+
+# client = MongoClient("localhost", 27017)
+# db = client.get_database("DATA")
+# auth = db.authenticate("twitterApplication", "gdotwitter", source="admin")
+# col = db.get_collection("BrexitRemain_old")
+
+client = MongoClient("146.169.32.151", 27017)
+db = client.get_database("DATA")
+auth = db.authenticate("twitterApplication", "gdotwitter", source="admin")
+col = db.get_collection("Twitter_Brexit_GNIP")
+
+sample_query = [
+    {"$match": {"user-id": {"$in": users}}},
+    {"$sample": {"size": 150000}},
+    {"$out": "GNIP_150000_sample_second1000users"}
+]
+
+print "Executing query"
+cursor = col.aggregate(sample_query, allowDiskUse=True)
+
+print "Done"
